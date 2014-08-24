a17fcaf09ac38b37dc96e7de058b1cc8417f8306
Alexander Nelzin <asnelzin@gmail.com>
2014-08-24 15:57:22 +0400

    Gunicorn start script changes.

--- deploy/scripts/gunicorn/start.sh
+++ deploy/scripts/gunicorn/start.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-set -e
+
+NAME="twitchscore"
 DJANGODIR=/home/asnelzin/twitchscore
 DJANGO_SETTINGS_MODULE=twitchscore.settings.production
-
+DJANGO_WSGI_MODULE=twitchscore.wsgi
 LOGFILE=/home/asnelzin/twitchscore/logs/gunicorn.log
-LOGDIR=$(dirname $LOGFILE)
 PIDFILE=/home/asnelzin/twitchscore/pids/gunicorn.pid
 NUM_WORKERS=3
-# user/group to run as
 USER=asnelzin
 GROUP=asnelzin
-cd /home/asnelzin/twitchscore
-source /home/asnelzin/.virtualenvs/twitchscore/bin/activate
 
+echo "Starting $NAME as `whoami`"
+
+source /home/asnelzin/.virtualenvs/twitchscore/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
-exec /home/asnelzin/.virtualenvs/twitchscore/bin/python /home/asnelzin/twitchscore/manage.py \
-  run_gunicorn --workers=$NUM_WORKERS \
-  --user=$USER --group=$GROUP --log-level=debug --pid=$PIDFILE \
-  --log-file=$LOGFILE --bind=localhost:8001
+exec /home/asnelzin/.virtualenvs/twitchscore/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+    --name=$NAME \
+    --workers=$NUM_WORKERS \
+    --user=$USER --group=$GROUP \
+    --bind=localhost:8001 \
+    --log-level=debug \
+    --log-file=$LOGFILE \
+    --pid=$PIDFILE
+
