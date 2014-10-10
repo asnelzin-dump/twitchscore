efcadf5872deee90d3b7b9da518df9c45741b9fc
Alexander Nelzin <asnelzin@gmail.com>
2014-10-10 19:34:25 +0400

    Use a real JS construct, WTF knows why this works in chromium.

--- twitchscore/apps/accounts/models.py
+++ twitchscore/apps/accounts/models.py
@@ -1,45 +1,29 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from twitchscore.apps.utils.riot import RiotAPI
+from django.contrib.auth.models import UserManager as DjangoUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+from twitchscore.apps.summoners.models import Summoner
 
 
-class Summoner(models.Model):
-    pass
-
-
-class User(AbstractBaseUser):
-    REGION_CHOICES = (
-        (1, 'BR'),
-        (2, 'EUNE'),
-        (3, 'EUW'),
-        (4, 'KR'),
-        (5, 'LAN'),
-        (6, 'LAS'),
-        (7, 'NA'),
-        (8, 'OCE'),
-        (9, 'RU'),
-        (10, 'TR'),
-    )
+class User(AbstractBaseUser, PermissionsMixin):
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
 
     email = models.EmailField(max_length=255, unique=True, db_index=True)
-    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    summoners = models.ManyToManyField(Summoner)
 
-    summoner_name = models.CharField(max_length=255)
-    summoner_id = models.IntegerField(blank=True)
-    region = models.CharField(max_length=3, choices=REGION_CHOICES)
+    is_active = models.BooleanField('Active?', default=False)
+    is_staff = models.BooleanField('Have admin access?', default=False)
+    date_joined = models.DateTimeField('When joined?', default=timezone.now)
+
+    objects = DjangoUserManager()
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+        ordering = ['email']
 
     def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.summoner_id:
-            riot_api = RiotAPI(self)
-            self.summoner_id = riot_api.get_id_by_name()
-        super(User, self).save(*args, **kwargs)
-
-
-
+        return self.username + ' (%s)' % self.email
